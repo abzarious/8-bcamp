03459d7dd5a25f13a7e4e1e8547358bd830c78e1
Abdullah Zakaria <kevjstein@gmail.com>
2026-05-20 18:23:06 +0700

    add: 4 routes and 1 blade page

--- 10-session/8bcamp-mall/routes/web.php
+++ 10-session/8bcamp-mall/routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products', function () {
+    return view('products');
+});
+
+Route::get('/cart', function () {
+    return view('cart');
+});
+
+Route::get('/checkout', function () {
+    return view('checkout');
+});
